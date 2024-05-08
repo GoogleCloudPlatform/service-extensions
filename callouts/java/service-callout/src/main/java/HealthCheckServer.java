621f7e168c2794b16d3f44bd325c3de80fc24b46
Vitor Resende <vresende@ciandt.com>
2024-05-08 14:35:05 -0300

    feat: creating env, health check server and service callout

---
+++ callouts/java/service-callout/src/main/java/HealthCheckServer.java
@@ -0,0 +1,42 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HealthCheckServer {
+
+    private final int port;
+    private final String path;
+    private final String ip;
+
+    public HealthCheckServer(int port, String path, String ip) {
+        this.port = port;
+        this.path = path;
+        this.ip = ip;
+    }
+
+    public void start() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
+        server.createContext(path, new HealthHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class HealthHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Server is healthy!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        HealthCheckServer healthCheckServer = new HealthCheckServer(8080, "", "0.0.0.0");
+        healthCheckServer.start();
+    }
+}
