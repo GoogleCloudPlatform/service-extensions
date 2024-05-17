9972471f53bedd0d9045e5ce1aea35a4c6811138
pweiber <periclesweiber@ciandt.com>
2024-05-17 13:37:20 -0300

    refactoring the handlers to use a handler registry | adding trailers handlers | updating dependencies

--- callouts/go/extproc/service/callout_server.go
+++ callouts/go/extproc/service/callout_server.go
@@ -1,5 +1,19 @@
-package main
- 
+// Copyright 2024 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package callout_server
+
 import (
 	"crypto/tls"
 	extproc "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
@@ -9,100 +23,80 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"os"
 )
- 
-func main() {
- 
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Current working directory:", dir)
- 
-	config := loadConfig()
-	server := NewCalloutServer(config)
-	go server.StartGRPC()
-	go server.StartInsecureGRPC()
-	go server.StartHealthCheck()
-	// Block forever or handle signals as needed
-	select {}
-}
- 
+
 func loadConfig() ServerConfig {
 	// Load configuration from environment variables or default
 	return ServerConfig{
 		Address:              "0.0.0.0:8443",
 		InsecureAddress:      "0.0.0.0:8181",
 		HealthCheckAddress:   "0.0.0.0:8000",
-		CertFile: 			  "extproc/ssl_creds/localhost.crt",
+		CertFile:             "extproc/ssl_creds/localhost.crt",
 		KeyFile:              "extproc/ssl_creds/localhost.key",
-		SecureHealthCheck:    true,
 		EnableInsecureServer: true,
 	}
 }
- 
+
 type ServerConfig struct {
 	Address              string
 	InsecureAddress      string
 	HealthCheckAddress   string
 	CertFile             string
 	KeyFile              string
-	SecureHealthCheck    bool
-	CombinedHealthCheck  bool
 	EnableInsecureServer bool
 }
- 
+
 type CalloutServer struct {
 	Config ServerConfig
 	Cert   tls.Certificate
 }
- 
+
 func NewCalloutServer(config ServerConfig) *CalloutServer {
-	cert, err := tls.LoadX509KeyPair(config.CertFile, config.KeyFile)
-	if err != nil {
-		log.Fatalf("Failed to load server certificate: %v", err)
+	var cert tls.Certificate
+	var err error
+
+	if config.CertFile != "" && config.KeyFile != "" {
+		cert, err = tls.LoadX509KeyPair(config.CertFile, config.KeyFile)
+		if err != nil {
+			log.Fatalf("Failed to load server certificate: %v", err)
+		}
 	}
+
 	return &CalloutServer{
 		Config: config,
 		Cert:   cert,
 	}
 }
- 
-func (s *CalloutServer) StartGRPC() {
+
+func (s *CalloutServer) StartGRPC(service extproc.ExternalProcessorServer) {
 	lis, err := net.Listen("tcp", s.Config.Address)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	creds := credentials.NewServerTLSFromCert(&s.Cert)
 	grpcServer := grpc.NewServer(grpc.Creds(creds))
-	extproc.RegisterExternalProcessorServer(grpcServer, &GRPCCalloutService{})
+	extproc.RegisterExternalProcessorServer(grpcServer, service)
 	reflection.Register(grpcServer)
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC: %v", err)
 	}
 }
- 
+
 func (s *CalloutServer) StartHealthCheck() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
- 
+
 	var server *http.Server = &http.Server{
 		Addr:    s.Config.HealthCheckAddress,
 		Handler: mux,
 	}
- 
-	if s.Config.SecureHealthCheck {
-		server.TLSConfig = &tls.Config{Certificates: []tls.Certificate{s.Cert}}
-		log.Fatal(server.ListenAndServeTLS("", "")) // Use the same cert as gRPC
-	} else {
-		log.Fatal(server.ListenAndServe())
-	}
+
+	log.Fatal(server.ListenAndServe())
 }
- 
-func (s *CalloutServer) StartInsecureGRPC() {
+
+func (s *CalloutServer) StartInsecureGRPC(service extproc.ExternalProcessorServer) {
 	if !s.Config.EnableInsecureServer {
 		return
 	}
@@ -111,40 +105,73 @@ func (s *CalloutServer) StartInsecureGRPC() {
 		log.Fatalf("Failed to listen on insecure port: %v", err)
 	}
 	grpcServer := grpc.NewServer() // No TLS credentials
-	extproc.RegisterExternalProcessorServer(grpcServer, &GRPCCalloutService{})
+	extproc.RegisterExternalProcessorServer(grpcServer, service)
 	reflection.Register(grpcServer)
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC on insecure port: %v", err)
 	}
 }
- 
+
+type RequestHeadersHandler func(*extproc.HttpHeaders) (*extproc.ProcessingResponse, error)
+type ResponseHeadersHandler func(*extproc.HttpHeaders) (*extproc.ProcessingResponse, error)
+type RequestBodyHandler func(*extproc.HttpBody) (*extproc.ProcessingResponse, error)
+type ResponseBodyHandler func(*extproc.HttpBody) (*extproc.ProcessingResponse, error)
+type RequestTrailersHandler func(*extproc.HttpTrailers) (*extproc.ProcessingResponse, error)
+type ResponseTrailersHandler func(*extproc.HttpTrailers) (*extproc.ProcessingResponse, error)
+
+type HandlerRegistry struct {
+	RequestHeadersHandler   RequestHeadersHandler
+	ResponseHeadersHandler  ResponseHeadersHandler
+	RequestBodyHandler      RequestBodyHandler
+	ResponseBodyHandler     ResponseBodyHandler
+	RequestTrailersHandler  RequestTrailersHandler
+	ResponseTrailersHandler ResponseTrailersHandler
+}
+
 type GRPCCalloutService struct {
 	extproc.UnimplementedExternalProcessorServer
+	Handlers HandlerRegistry
 }
- 
+
 func (s *GRPCCalloutService) Process(stream extproc.ExternalProcessor_ProcessServer) error {
 	for {
 		req, err := stream.Recv()
 		if err != nil {
 			return err
 		}
- 
+
 		var response *extproc.ProcessingResponse
 		switch {
 		case req.GetRequestHeaders() != nil:
-			response, err = s.handleRequestHeaders(req.GetRequestHeaders())
+			if s.Handlers.RequestHeadersHandler != nil {
+				response, err = s.Handlers.RequestHeadersHandler(req.GetRequestHeaders())
+			}
 		case req.GetResponseHeaders() != nil:
-			response, err = s.handleResponseHeaders(req.GetResponseHeaders())
+			if s.Handlers.ResponseHeadersHandler != nil {
+				response, err = s.Handlers.ResponseHeadersHandler(req.GetResponseHeaders())
+			}
 		case req.GetRequestBody() != nil:
-			response, err = s.handleRequestBody(req.GetRequestBody())
+			if s.Handlers.RequestBodyHandler != nil {
+				response, err = s.Handlers.RequestBodyHandler(req.GetRequestBody())
+			}
 		case req.GetResponseBody() != nil:
-			response, err = s.handleResponseBody(req.GetResponseBody())
+			if s.Handlers.ResponseBodyHandler != nil {
+				response, err = s.Handlers.ResponseBodyHandler(req.GetResponseBody())
+			}
+		case req.GetRequestTrailers() != nil:
+			if s.Handlers.RequestTrailersHandler != nil {
+				response, err = s.Handlers.RequestTrailersHandler(req.GetRequestTrailers())
+			}
+		case req.GetResponseTrailers() != nil:
+			if s.Handlers.ResponseTrailersHandler != nil {
+				response, err = s.Handlers.ResponseTrailersHandler(req.GetResponseTrailers())
+			}
 		}
- 
+
 		if err != nil {
 			return err
 		}
- 
+
 		if response != nil {
 			if err := stream.Send(response); err != nil {
 				return err
@@ -152,35 +179,51 @@ func (s *GRPCCalloutService) Process(stream extproc.ExternalProcessor_ProcessSer
 		}
 	}
 }
- 
-func (s *GRPCCalloutService) handleRequestHeaders(headers *extproc.HttpHeaders) (*extproc.ProcessingResponse, error) {
+
+func (s *GRPCCalloutService) HandleRequestHeaders(headers *extproc.HttpHeaders) (*extproc.ProcessingResponse, error) {
 	return &extproc.ProcessingResponse{
 		Response: &extproc.ProcessingResponse_RequestHeaders{
 			RequestHeaders: &extproc.HeadersResponse{},
 		},
 	}, nil
 }
- 
-func (s *GRPCCalloutService) handleResponseHeaders(headers *extproc.HttpHeaders) (*extproc.ProcessingResponse, error) {
+
+func (s *GRPCCalloutService) HandleResponseHeaders(headers *extproc.HttpHeaders) (*extproc.ProcessingResponse, error) {
 	return &extproc.ProcessingResponse{
 		Response: &extproc.ProcessingResponse_ResponseHeaders{
 			ResponseHeaders: &extproc.HeadersResponse{},
 		},
 	}, nil
 }
- 
-func (s *GRPCCalloutService) handleRequestBody(body *extproc.HttpBody) (*extproc.ProcessingResponse, error) {
+
+func (s *GRPCCalloutService) HandleRequestBody(body *extproc.HttpBody) (*extproc.ProcessingResponse, error) {
 	return &extproc.ProcessingResponse{
 		Response: &extproc.ProcessingResponse_RequestBody{
 			RequestBody: &extproc.BodyResponse{},
 		},
 	}, nil
 }
- 
-func (s *GRPCCalloutService) handleResponseBody(body *extproc.HttpBody) (*extproc.ProcessingResponse, error) {
+
+func (s *GRPCCalloutService) HandleResponseBody(body *extproc.HttpBody) (*extproc.ProcessingResponse, error) {
 	return &extproc.ProcessingResponse{
 		Response: &extproc.ProcessingResponse_ResponseBody{
 			ResponseBody: &extproc.BodyResponse{},
+		},
+	}, nil
+}
+
+func (s *GRPCCalloutService) HandleRequestTrailers(trailers *extproc.HttpTrailers) (*extproc.ProcessingResponse, error) {
+	return &extproc.ProcessingResponse{
+		Response: &extproc.ProcessingResponse_RequestTrailers{
+			RequestTrailers: &extproc.TrailersResponse{},
+		},
+	}, nil
+}
+
+func (s *GRPCCalloutService) HandleResponseTrailers(trailers *extproc.HttpTrailers) (*extproc.ProcessingResponse, error) {
+	return &extproc.ProcessingResponse{
+		Response: &extproc.ProcessingResponse_ResponseTrailers{
+			ResponseTrailers: &extproc.TrailersResponse{},
 		},
 	}, nil
 }
--- callouts/go/go.mod
+++ callouts/go/go.mod
@@ -1,19 +1,24 @@
 module service-extensions-samples
- 
+
 require (
 	github.com/envoyproxy/go-control-plane v0.12.0
 	google.golang.org/grpc v1.63.2
 )
- 
+
 require (
 	github.com/cncf/xds/go v0.0.0-20231128003011-0fa0005c9caa // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/envoyproxy/protoc-gen-validate v1.0.4 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
- 
+
 go 1.22
