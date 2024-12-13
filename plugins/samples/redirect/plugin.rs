24f03e4844bd298c64ca2214be9784f3d6b28589
victorwoli <victorwo@ciandt.com>
2024-12-13 13:30:03 -0300

    Removing unnecessary comments

--- plugins/samples/redirect/plugin.rs
+++ plugins/samples/redirect/plugin.rs
@@ -11,46 +11,8 @@
 // // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // // See the License for the specific language governing permissions and
 // // limitations under the License.a
-//
-// use proxy_wasm::traits::*;
-// use proxy_wasm::types::*;
-//
-// const OLD_PATH_PREFIX: &str = "/foo/";
-// const NEW_PATH_PREFIX: &str = "/bar/";
-//
-// struct MyHttpContext;
-//
-// impl Context for MyHttpContext {}
-//
-// impl HttpContext for MyHttpContext {
-//     fn on_http_request_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
-//         // Get the ":path" header value.
-//         if let Some(path) = self.get_http_request_header(":path") {
-//             // Check if the path starts with the OLD_PATH_PREFIX.
-//             if path.starts_with(OLD_PATH_PREFIX) {
-//                 // Construct the new path by replacing the prefix.
-//                 let new_path = format!("{}{}", NEW_PATH_PREFIX, &path[OLD_PATH_PREFIX.len()..]);
-//
-//                 // Send a 301 response with the new location.
-//                 self.send_http_response(
-//                     301,
-//                     vec![("Location", new_path.as_str())],
-//                     Some(format!("Content moved to {}", new_path).as_bytes()),
-//                 );
-//
-//                 return Action::Pause; // End stream and stop further processing.
-//             }
-//         }
-//         Action::Continue
-//     }
-// }
-//
-// // Register the context factory.
-// proxy_wasm::main! {
-//     proxy_wasm::set_http_context(|_, _| -> Box<dyn HttpContext> {
-//         Box::new(MyHttpContext)
-//     });
-// }
+
+// [START serviceextensions_plugin_redirect]
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
 
@@ -88,3 +50,4 @@ impl HttpContext for MyHttpContext {
         Action::Continue
     }
 }
+// [END serviceextensions_plugin_redirect]
