6932b6d999c94db07c270a45e5deae0af3e28275
Martijn Stevenson <mstevenson@google.com>
2024-12-14 19:41:43 +0000

    Add license blocks and content tags.
    Signed-off-by: Martijn Stevenson <mstevenson@google.com>

--- plugins/samples/docs_first_plugin/plugin.cc
+++ plugins/samples/docs_first_plugin/plugin.cc
@@ -1,3 +1,18 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START serviceextensions_plugin_docs_first_plugin]
 #include "proxy_wasm_intrinsics.h"
 
 class MyHttpContext : public Context {
@@ -26,3 +41,4 @@ class MyHttpContext : public Context {
 
 static RegisterContextFactory register_StaticContext(
     CONTEXT_FACTORY(MyHttpContext), ROOT_FACTORY(RootContext));
+// [END serviceextensions_plugin_docs_first_plugin]
