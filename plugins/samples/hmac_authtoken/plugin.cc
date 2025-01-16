216dc22f9238cad85d3abc4d6f3f3d3326b7b3e6
Weslley Alves <walves@ciandt.com>
2025-01-16 18:38:56 -0300

    fix: changing to use absl::BytesToHexString function and removing some auto variables to more explicity type

--- plugins/samples/hmac_authtoken/plugin.cc
+++ plugins/samples/hmac_authtoken/plugin.cc
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,10 +17,9 @@
 
 #include <boost/url/parse.hpp>
 #include <boost/url/url.hpp>
-#include <iomanip>
-#include <sstream>
 #include <string>
 
+#include "absl/strings/escaping.h"
 #include "proxy_wasm_intrinsics.h"
 
 // Replace with your desired secret key.
@@ -42,10 +41,10 @@ class MyHttpContext : public Context {
       return FilterHeadersStatus::ContinueAndEndStream;
     }
 
-    const auto token = (*it).value;
+    const std::string token = (*it).value;
     // Strip the token from the URL.
     url->params().erase(it);
-    const auto path = url->buffer();
+    const std::string path = url->buffer();
     // Compare if the generated signature matches the token sent.
     // In this sample the signature is generated using the request :path.
     if (computeHmacSignature(path) != token) {
@@ -59,15 +58,6 @@ class MyHttpContext : public Context {
   }
 
  private:
-  // Helper function to convert binary data to a hexadecimal string.
-  std::string toHexString(const unsigned char* data, size_t length) {
-    std::stringstream ss;
-    for (size_t i = 0; i < length; ++i) {
-      ss << std::hex << std::setw(2) << std::setfill('0') << (int)data[i];
-    }
-    return ss.str();
-  }
-
   // Function to compute the HMAC signature.
   std::string computeHmacSignature(const std::string& data) {
     unsigned char* result;
@@ -76,7 +66,7 @@ class MyHttpContext : public Context {
     result = HMAC(EVP_sha256(), kSecretKey.c_str(), kSecretKey.length(),
                   reinterpret_cast<const unsigned char*>(data.c_str()),
                   data.length(), nullptr, &len);
-    return toHexString(result, len);
+    return absl::BytesToHexString(reinterpret_cast<const char*>(result));
   }
 };
 
