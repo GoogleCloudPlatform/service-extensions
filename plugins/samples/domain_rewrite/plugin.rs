f796f1bc8a4b58545b12be79e52cf1fb6f7f5afa
dallen68 <dantebot015@gmail.com>
2025-01-27 09:19:20 -0500

    Add license

--- plugins/samples/domain_rewrite/plugin.rs
+++ plugins/samples/domain_rewrite/plugin.rs
@@ -1,3 +1,18 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START serviceextensions_plugin_domain_rewrite]
 use lol_html::*;
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
@@ -76,3 +91,4 @@ impl<'a> HttpContext for MyHttpContext<'a> {
         return Action::Continue;
     }
 }
+// [END serviceextensions_plugin_domain_rewrite]
