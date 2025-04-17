796899bc432c4eeb19abfde8346a78a0cd14dc0e
dallen68 <dantebot015@gmail.com>
2025-04-17 09:33:48 -0400

    fix: Prevent additional callbacks after immediate response

--- plugins/test/framework.cc
+++ plugins/test/framework.cc
@@ -191,19 +191,24 @@ TestHttpContext::Result TestHttpContext::SendRequestHeaders(
 TestHttpContext::Result TestHttpContext::SendRequestBody(std::string body) {
   phase_logs_.clear();
   result_ = Result{};
+  if (sent_local_response_) {
+    return Result{};
+  }
   body_buffer_.setOwned(std::move(body));
   current_callback_ = TestHttpContext::CallbackType::RequestBody;
   result_.body_status =
       onRequestBody(body_buffer_.size(), /*end_of_stream=*/false);
-  if (!sent_local_response_) {
     result_.body = body_buffer_.release();
-  }
   return std::move(result_);
 }
 
 TestHttpContext::Result TestHttpContext::SendResponseHeaders(
     TestHttpContext::Headers headers) {
   phase_logs_.clear();
+  result_ = Result{};
+  if (sent_local_response_) {
+    return Result{};
+  }
   result_ = Result{.headers = std::move(headers)};
   phase_ = proxy_wasm::WasmHeaderMapType::ResponseHeaders;
   current_callback_ = TestHttpContext::CallbackType::ResponseHeaders;
@@ -216,6 +221,9 @@ TestHttpContext::Result TestHttpContext::SendResponseHeaders(
 TestHttpContext::Result TestHttpContext::SendResponseBody(std::string body) {
   phase_logs_.clear();
   result_ = Result{};
+  if (sent_local_response_) {
+    return Result{};
+  }
   body_buffer_.setOwned(std::move(body));
   current_callback_ = TestHttpContext::CallbackType::ResponseBody;
   result_.body_status =
