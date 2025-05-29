c7bf89c2ad0067c829f8bfaff5e9eb6be4eb8147
victorwoli <victorwo@ciandt.com>
2025-05-29 13:39:02 -0300

    Adding hello world plugin sample for C++, GO and RUST

---
+++ plugins/samples/hello_world/plugin.cc
@@ -0,0 +1,44 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [START serviceextensions_plugin_hello_world]
+#include "proxy_wasm_intrinsics.h"
+class HelloWorldRootContext : public RootContext {
+public:
+  explicit HelloWorldRootContext(uint32_t id, std::string_view root_id) : RootContext(id, root_id) {}
+};
+class HelloWorldHttpContext : public Context {
+public:
+  explicit HelloWorldHttpContext(uint32_t id, RootContext* root) : Context(id, root) {}
+  FilterHeadersStatus onRequestHeaders(uint32_t, bool) override {
+    return FilterHeadersStatus::Continue;
+  }
+  FilterDataStatus onRequestBody(size_t body_size, bool end_stream) override {
+    return FilterDataStatus::Continue;
+  }
+  FilterHeadersStatus onResponseHeaders(uint32_t headers,
+                                        bool end_of_stream) override {
+    // Add the :status header explicitly
+    addResponseHeader(":status", "200");
+    addResponseHeader("Content-Type", "text/plain");
+    
+    // Send a local response with status 200 and "Hello World" as body
+    sendLocalResponse(200, "Hello World", "",
+                      {{"Content-Type", "text/plain"}, {":status", "200"}});
+    return FilterHeadersStatus::StopIteration;
+}
+};
+static RegisterContextFactory register_HelloWorldContext(
+    CONTEXT_FACTORY(HelloWorldHttpContext),
+    ROOT_FACTORY(HelloWorldRootContext));
+// [END serviceextensions_plugin_hello_world]
---
+++ plugins/samples/hello_world/plugin.go
@@ -0,0 +1,67 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START serviceextensions_plugin_hello_world]
+package main
+
+import (
+	"github.com/proxy-wasm/proxy-wasm-go-sdk/proxywasm"
+	"github.com/proxy-wasm/proxy-wasm-go-sdk/proxywasm/types"
+)
+
+func main() {}
+
+func init() {
+	proxywasm.SetVMContext(&vmContext{})
+}
+
+type vmContext struct {
+	types.DefaultVMContext
+}
+
+type pluginContext struct {
+	types.DefaultPluginContext
+}
+
+type httpContext struct {
+	types.DefaultHttpContext
+}
+
+func (vc *vmContext) NewPluginContext(contextID uint32) types.PluginContext {
+	return &pluginContext{}
+}
+
+func (pc *pluginContext) NewHttpContext(contextID uint32) types.HttpContext {
+	return &httpContext{}
+}
+
+func (ctx *httpContext) OnHttpRequestHeaders(numHeaders int, endOfStream bool) types.Action {
+	// Instead of sending response immediately, let's modify the response
+	return types.ActionContinue
+}
+
+func (ctx *httpContext) OnHttpResponseHeaders(numHeaders int, endOfStream bool) types.Action {
+	// Set response headers
+	proxywasm.ReplaceHttpResponseHeader("Content-Type", "text/plain")
+	proxywasm.ReplaceHttpResponseHeader(":status", "200")
+	return types.ActionContinue
+}
+
+func (ctx *httpContext) OnHttpResponseBody(bodySize int, endOfStream bool) types.Action {
+	// Replace the response body with "Hello World"
+	proxywasm.ReplaceHttpResponseBody([]byte("Hello World"))
+	return types.ActionContinue
+}
+
+// [END serviceextensions_plugin_hello_world]
