f5c6b5b279867cd569a9cbfc5a983a6741c6f242
israel4435-beep <israel4435@gmail.com>
2025-08-27 16:10:32 -0700

    Delete callouts directory

--- callouts/python/protodef/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-import glob
-
-package_list = ['envoy', 'udpa', 'validate', 'xds']
-glob_list = [glob.glob(f'{glob_dir}/**/', recursive=True) for glob_dir in package_list]
-for glob_dirs in glob_list:
-    package_list += glob_dirs
-setup(name='protodef', version='1.0.0', packages=package_list)
